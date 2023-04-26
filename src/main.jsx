d7de646a1869a39668a8c97bcd39b1bc73204aa1
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-04-26 20:28:31 -0300

    Arrumada em import duplicado

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { ColorModeScript } from "@chakra-ui/react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
