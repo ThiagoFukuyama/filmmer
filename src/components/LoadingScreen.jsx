64185de1b834bfa4c235ed97f636fcccab058649
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-06-16 01:36:09 -0300

    Ajustes em classes de componentes

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import { LuLoader2 } from "react-icons/lu";
 const LoadingScreen = () => {
     return (
         <div className="bg-dark-gray-200 absolute inset-0 flex justify-center z-10">
-            <LuLoader2 color="#DD4849" className="animate-spin" size={40} />
+            <LuLoader2 color="#DD4849" size={40} className="animate-spin" />
         </div>
     );
 };
