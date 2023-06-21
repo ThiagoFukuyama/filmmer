60c785cca2ce1ac1f67d98ba08adbd72b7142279
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-06-21 19:11:29 -0300

    Colocada imagem placeholder ao erro no card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,17 +2,15 @@ import FavoriteButton from "./FavoriteButton";
 
 const MovieCard = ({ movie }) => {
     const { Title, Year, Poster, Type } = movie;
-
+    const PLACEHOLDER_IMAGE_URL = "https://via.placeholder.com/400";
     let typeColor;
     switch (Type) {
         case "series":
             typeColor = "bg-[#A6378F]";
             break;
-
         case "game":
             typeColor = "bg-[#CF445B]";
             break;
-
         default:
             typeColor = "bg-[#E74C3C]";
             break;
@@ -22,11 +20,8 @@ const MovieCard = ({ movie }) => {
         <div className="relative w-[300px] min-h-[500px] bg-dark-gray-100 rounded-md overflow-hidden transition duration-300 hover:scale-[1.025]">
             <img
                 className="w-full h-[300px] object-cover"
-                src={
-                    Poster !== "N/A"
-                        ? Poster
-                        : "https://via.placeholder.com/400"
-                }
+                src={Poster !== "N/A" ? Poster : PLACEHOLDER_IMAGE_URL}
+                onError={(e) => (e.target.src = PLACEHOLDER_IMAGE_URL)}
             />
 
             <div className="p-5">
