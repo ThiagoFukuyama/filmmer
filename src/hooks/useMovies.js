ef2da94705a6e527c308f9d44aa62d6b1a65dff3
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-06-22 18:10:50 -0300

    Novo useMovies hook

---
+++ src/hooks/useMovies.js
@@ -0,0 +1,23 @@
+import { useState, useMemo } from "react";
+import { useDebounce } from "../hooks/useDebounce";
+
+export const useMovies = () => {
+    const [movies, setMovies] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    const searchMovie = (title = "") => {
+        fetch(`https://www.omdbapi.com?apikey=5dfc069&s=${title || "Sword"}`)
+            .then((response) => {
+                if (!response.ok) throw new Error();
+                return response.json();
+            })
+            .then((data) => setMovies(data.Search))
+            .catch(() => setError(true))
+            .finally(() => setIsLoading(false));
+    };
+
+    const debouncedSearchMovie = useMemo(() => useDebounce(searchMovie), []);
+
+    return { movies, isLoading, error, searchMovie, debouncedSearchMovie };
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect, useMemo } from "react";
-import { useDebounce } from "../hooks/useDebounce";
+import { useState, useEffect } from "react";
+import { useMovies } from "../hooks/useMovies";
 import Heading from "../components/Heading";
 import Container from "../components/Container";
 import MainTitle from "../components/MainTitle";
@@ -8,23 +8,9 @@ import LoadingScreen from "../components/LoadingScreen";
 import MoviesContainer from "../components/MoviesContainer";
 
 const Home = () => {
-    const [movies, setMovies] = useState([]);
     const [searchQuery, setSearchQuery] = useState("");
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(false);
-
-    const searchMovie = (title = "") => {
-        fetch(`https://www.omdbapi.com?apikey=5dfc069&s=${title || "Sword"}`)
-            .then((response) => {
-                if (!response.ok) throw new Error();
-                return response.json();
-            })
-            .then((data) => setMovies(data.Search))
-            .catch(() => setError(true))
-            .finally(() => setIsLoading(false));
-    };
-
-    const debouncedSearchMovie = useMemo(() => useDebounce(searchMovie), []);
+    const { movies, isLoading, error, searchMovie, debouncedSearchMovie } =
+        useMovies();
 
     useEffect(() => {
         searchMovie();
