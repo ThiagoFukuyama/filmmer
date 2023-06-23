6f480282f4f188caa322c3cd8cd00fe8d5e4167b
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-06-23 15:49:12 -0300

    z-index da LoadingScreen aumentado

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { LuLoader2 } from "react-icons/lu";
 
 const LoadingScreen = () => {
     return (
-        <div className="bg-dark-gray-200 absolute inset-0 flex justify-center z-10">
+        <div className="bg-dark-gray-200 absolute inset-0 flex justify-center z-20">
             <LuLoader2 color="#DD4849" size={40} className="animate-spin" />
         </div>
     );
