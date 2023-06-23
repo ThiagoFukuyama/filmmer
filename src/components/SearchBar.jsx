716f7c03ce5829904ccd9be2fb27e01c2b5f1a31
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-06-23 15:17:57 -0300

    Novo botão de limpar barra de navegação

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,28 +1,41 @@
 import { useState } from "react";
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
 
-const SearchBar = ({ onChange }) => {
+const SearchBar = ({ onChange: search }) => {
     const [searchQuery, setSearchQuery] = useState("");
 
     const handleOnChange = ({ target: { value } }) => {
         setSearchQuery(value);
-        onChange(value.trim());
+        search(value.trim());
+    };
+
+    const handleClick = () => {
+        setSearchQuery("");
+        search("");
     };
 
     return (
         <div className="relative max-w-[660px] bg-dark-gray-100 focus-within:bg-dark-gray-200 border border-transparent focus-within:border-[#DD484990] text-lg shadow-2xl rounded-full mx-auto mb-20 px-3 transition duration-200">
             <AiOutlineSearch
-                className="absolute left-4 bottom-3"
+                className="absolute left-4 bottom-3 pointer-events-none"
                 size={28}
                 color="#DD4849"
             />
             <input
-                className="w-full bg-transparent outline-none pl-10 pr-3 py-3 placeholder:opacity-40"
+                className="w-full bg-transparent outline-none pl-10 pr-8 py-3 placeholder:opacity-40"
                 type="text"
                 placeholder="Search for movies, series and games..."
                 value={searchQuery}
                 onChange={handleOnChange}
             />
+            {searchQuery !== "" && (
+                <button
+                    onClick={handleClick}
+                    className="absolute right-5 bottom-1/3"
+                >
+                    <AiOutlineClose size={18} color="#DD4849" />
+                </button>
+            )}
         </div>
     );
 };
