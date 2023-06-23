77454c5322292013626f259099891f6aa57cbe8d
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-06-23 14:11:56 -0300

    MoviesContainer refatorado

--- src/components/MoviesContainer.jsx
+++ src/components/MoviesContainer.jsx
@@ -2,15 +2,16 @@ import Heading from "./Heading";
 import MovieCard from "./MovieCard";
 
 const MoviesContainer = ({ movies, emptyMessage }) => {
-    return movies?.length > 0 ? (
-        <div className="flex flex-wrap justify-center gap-8 mb-10">
-            {movies.map((movie) => (
-                <MovieCard key={movie.imdbID} movie={movie} />
-            ))}
-        </div>
-    ) : (
-        <Heading className="text-center">{emptyMessage}</Heading>
-    );
+    if (movies?.length > 0)
+        return (
+            <div className="flex flex-wrap justify-center gap-8 mb-10">
+                {movies.map((movie) => (
+                    <MovieCard key={movie.imdbID} movie={movie} />
+                ))}
+            </div>
+        );
+
+    return <Heading className="text-center">{emptyMessage}</Heading>;
 };
 
 export default MoviesContainer;
