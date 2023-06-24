672e3e7e9d93881e095d8bbf2e19fdc0489d6826
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-06-23 23:31:01 -0300

    Imports arrumados para intellisense

--- src/components/MoviesContainer.jsx
+++ src/components/MoviesContainer.jsx
@@ -1,5 +1,5 @@
-import { Heading } from "components/shared";
-import MovieCard from "components/MovieCard";
+import { Heading } from "./shared";
+import MovieCard from "./MovieCard";
 
 const MoviesContainer = ({ movies, emptyMessage }) => {
     if (movies?.length > 0)
--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    resolve: {
-        alias: {
-            src: "/src",
-            components: "/src/components",
-            context: "/src/context",
-            data: "/src/data",
-            hooks: "/src/hooks",
-            pages: "/src/pages",
-        },
-    },
 });
