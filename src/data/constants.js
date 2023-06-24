06eae064676e18f4fd293770bbabfc991235f5d1
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-06-24 00:14:24 -0300

    Object.freeze() em constante

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,7 +1,7 @@
 export const PLACEHOLDER_IMAGE_URL = "https://via.placeholder.com/400";
 
-export const MEDIA_TYPE_COLORS = {
+export const MEDIA_TYPE_COLORS = Object.freeze({
     movie: "bg-movie",
     series: "bg-series",
     game: "bg-game",
-};
+});
