2cf76a4e6182cbec5faa68c8b149d3ff10abfb3f
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-06-28 17:36:14 -0300

    Alteradas as classes do MainTitle

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -1,7 +1,7 @@
 const MainTitle = ({ children }) => {
     return (
-        <div className="flex justify-center items-center mt-12 mb-8">
-            <h1 className="text-5xl md:text-6xl uppercase font-bold text-center text-transparent bg-clip-text bg-filmmer-gradient">
+        <div className="text-center">
+            <h1 className="inline-block text-5xl md:text-6xl uppercase font-bold text-transparent bg-clip-text bg-filmmer-gradient mt-12 mb-8">
                 {children}
             </h1>
         </div>
